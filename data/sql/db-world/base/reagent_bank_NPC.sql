6d7f5a8dabf2dc1f1c04fb7882878e94965f14d4
Rafail Kagarmanov <104055624+MRafStudio@users.noreply.github.com>
2023-11-16 19:05:40 +0600

    Added ruRU locale

--- data/sql/db-world/base/reagent_bank_NPC.sql
+++ data/sql/db-world/base/reagent_bank_NPC.sql
@@ -4,6 +4,8 @@ SET
 @Name = "Ling";
 
 DELETE FROM `creature_template` WHERE `entry` = @Entry;
-
 INSERT INTO `creature_template` (`entry`, `modelid1`, `modelid2`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `scale`, `rank`, `dmgschool`, `baseattacktime`, `rangeattacktime`, `unit_class`, `unit_flags`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `AIName`, `MovementType`, `HoverHeight`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `flags_extra`, `ScriptName`) VALUES
 (@Entry, 15965, 0, @Name, 'Reagent Banker', NULL, 0, 6, 6, 0, 35, 1, 1, 0, 0, 2000, 0, 1, 0, 7, 138412032, 0, 0, 0, '', 0, 1, 0, 0, 1, 0, 2, 'npc_reagent_banker');
+
+DELETE FROM `creature_template_locale` WHERE `entry` = @Entry;
+INSERT INTO `creature_template_locale` (`entry`, `locale`, `Name`, `Title`) VALUES (@Entry, 'ruRU', 'Банк реагентов', '');
